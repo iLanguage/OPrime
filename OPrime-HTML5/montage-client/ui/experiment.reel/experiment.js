791f6a8ab8a1893d43a6091b91f04496e0cdd4d8
cesine <cesine@yahoo.com>
2013-09-30 20:30:54 -0400

    adding details to experiment

--- OPrime-HTML5/montage-client/ui/experiment.reel/experiment.js
+++ OPrime-HTML5/montage-client/ui/experiment.reel/experiment.js
@@ -8,10 +8,31 @@ var Component = require("montage/ui/component").Component;
  * @class Experiment
  * @extends Component
  */
-exports.Experiment = Component.specialize(/** @lends Experiment# */ {
+exports.Experiment = Component.specialize( /** @lends Experiment# */ {
     constructor: {
         value: function Experiment() {
             this.super();
         }
+    },
+    title: {
+        value: "Experiment title"
+    },
+    description: {
+        value: "Experiment description"
+    },
+    experimenters: {
+        value: [{
+            username: "experimenter",
+            gravatar: "3b2aea0bec2e4e6588c61eaa1c6e05e0"
+        }, {
+            username: "assistant"
+        }, {
+            username: "observer"
+        }]
+    },
+    participants: {
+        value: [{
+            username: "participant1"
+        }]
     }
 });
