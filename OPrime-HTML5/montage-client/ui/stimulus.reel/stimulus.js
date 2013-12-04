161e2027c58d23d72730862340c0618c9784d0ca
cesine <cesine@yahoo.com>
2013-12-04 11:32:53 -0500

    added stimulus for 4 images, prime and target

---
+++ OPrime-HTML5/montage-client/ui/stimulus.reel/stimulus.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/stimulus.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Stimulus
+ * @extends Component
+ */
+exports.Stimulus = Component.specialize(/** @lends Stimulus# */ {
+    constructor: {
+        value: function Stimulus() {
+            this.super();
+        }
+    }
+});
