c0f8e0e4fb947f3f78e0933f383c94fc477ab557
DominiqueBedard <dominique.bedard@umontreal.ca>
2013-12-04 18:20:45 -0500

    créé un module à une image

---
+++ OPrime-HTML5/montage-client/ui/one-image-stimulus.reel/one-image-stimulus.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/one-image-stimulus.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class OneImageStimulus
+ * @extends Component
+ */
+exports.OneImageStimulus = Component.specialize(/** @lends OneImageStimulus# */ {
+    constructor: {
+        value: function OneImageStimulus() {
+            this.super();
+        }
+    }
+});
