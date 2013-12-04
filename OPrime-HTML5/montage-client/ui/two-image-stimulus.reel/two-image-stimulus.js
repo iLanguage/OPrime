4145259aaf70fd6698aec237ae6ec1c76d27e40d
DominiqueBedard <dominique.bedard@umontreal.ca>
2013-12-04 17:42:32 -0500

    créé un module à deux images

---
+++ OPrime-HTML5/montage-client/ui/two-image-stimulus.reel/two-image-stimulus.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/two-image-stimulus.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TwoImageStimulus
+ * @extends Component
+ */
+exports.TwoImageStimulus = Component.specialize(/** @lends TwoImageStimulus# */ {
+    constructor: {
+        value: function TwoImageStimulus() {
+            this.super();
+        }
+    }
+});
