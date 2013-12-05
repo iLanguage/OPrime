89b510b7644c090791a5d57a4d638ab7bfa76bf5
cesine <cesine@yahoo.com>
2013-12-05 14:48:25 -0500

    typo when creating four image stimulus

---
+++ OPrime-HTML5/montage-client/ui/stimulus.reel/four-image-stimulus.reel/four-image-stimulus.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/four-image-stimulus.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FourImageStimulus
+ * @extends Component
+ */
+exports.FourImageStimulus = Component.specialize(/** @lends FourImageStimulus# */ {
+    constructor: {
+        value: function FourImageStimulus() {
+            this.super();
+        }
+    }
+});
