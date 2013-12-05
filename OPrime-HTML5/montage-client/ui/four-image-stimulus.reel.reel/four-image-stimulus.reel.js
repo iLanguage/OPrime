935a0a5822e54cf59badb4a57ea5f3e211de94b5
cesine <cesine@yahoo.com>
2013-12-05 12:22:10 -0500

    creating a component to move the 4 image stimulus into

---
+++ OPrime-HTML5/montage-client/ui/four-image-stimulus.reel.reel/four-image-stimulus.reel.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/four-image-stimulus.reel.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FourImageStimulus.reel
+ * @extends Component
+ */
+exports.FourImageStimulus.reel = Component.specialize(/** @lends FourImageStimulus.reel# */ {
+    constructor: {
+        value: function FourImageStimulus.reel() {
+            this.super();
+        }
+    }
+});
