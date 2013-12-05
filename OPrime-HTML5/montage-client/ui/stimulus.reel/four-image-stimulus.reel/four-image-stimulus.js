610251fdeae7bdc19be986dfe66dce97a546e99c
cesine <cesine@yahoo.com>
2013-12-05 18:14:01 -0500

    moved 4 image stimulus into four-image-stimulus and other clean up

--- OPrime-HTML5/montage-client/ui/stimulus.reel/four-image-stimulus.reel/four-image-stimulus.js
+++ OPrime-HTML5/montage-client/ui/stimulus.reel/four-image-stimulus.reel/four-image-stimulus.js
@@ -2,16 +2,18 @@
  * @module ui/four-image-stimulus.reel
  * @requires montage/ui/component
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+	Stimulus = require("ui/stimulus.reel").Stimulus;
 
 /**
  * @class FourImageStimulus
- * @extends Component
+ * @extends Stimulus
  */
-exports.FourImageStimulus = Component.specialize(/** @lends FourImageStimulus# */ {
-    constructor: {
-        value: function FourImageStimulus() {
-            this.super();
-        }
-    }
+exports.FourImageStimulus = Stimulus.specialize( /** @lends FourImageStimulus# */ {
+	constructor: {
+		value: function FourImageStimulus() {
+			this.super();
+			this.primeImage = "http://fc09.deviantart.net/fs71/f/2012/338/2/1/wat__dash_by_reallyunimportant-d5gl3y0.png";
+		}
+	}
 });
--- OPrime-HTML5/montage-client/ui/stimulus.reel/stimulus.js
+++ OPrime-HTML5/montage-client/ui/stimulus.reel/stimulus.js
@@ -8,10 +8,11 @@ var Component = require("montage/ui/component").Component;
  * @class Stimulus
  * @extends Component
  */
-exports.Stimulus = Component.specialize(/** @lends Stimulus# */ {
-    constructor: {
-        value: function Stimulus() {
-            this.super();
-        }
-    }
+exports.Stimulus = Component.specialize( /** @lends Stimulus# */ {
+	constructor: {
+		value: function Stimulus() {
+			this.super();
+			this.primeImage = "http://th00.deviantart.net/fs70/PRE/i/2012/316/a/c/aj__dash_and_rarity_by_reallyunimportant-d5knc6e.png";
+		}
+	}
 });
