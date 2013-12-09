69ef2a166e32b842abc95c145de7b9c08f861e7f
cesine <cesine@yahoo.com>
2013-12-09 12:19:38 -0500

    added responses

--- OPrime-HTML5/montage-client/ui/experiment.reel/experiment.js
+++ OPrime-HTML5/montage-client/ui/experiment.reel/experiment.js
@@ -21,19 +21,6 @@ exports.Experiment = Component.specialize( /** @lends Experiment# */ {
 	constructor: {
 		value: function Experiment() {
 			this.super();
-			this._currentStimulus = {
-				"auditoryStimuli": "auditory_stimuli_1",
-				"audioFile": "1.wav",
-				"primeImage": "animal1.png",
-				"targetImage": "04_scie.png",
-				"distractorImages": ["01_feu.png", "03_but.png", "02_chat.png"],
-				"responses": [{
-					"reactionTimeAudioOffset": null,
-					"reactionTimeAudioOnset": null,
-					"x": null,
-					"y": null
-				}]
-			};
 		}
 	},
 
---
+++ OPrime-HTML5/montage-client/ui/response.reel/response.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/response.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Response
+ * @extends Component
+ */
+exports.Response = Component.specialize(/** @lends Response# */ {
+    constructor: {
+        value: function Response() {
+            this.super();
+        }
+    }
+});
