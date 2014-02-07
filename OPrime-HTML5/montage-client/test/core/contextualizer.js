c2d8ce1dbd0163f1c79664131ef66ba517ba3634
cesine <cesine@yahoo.com>
2014-02-07 15:04:33 -0500

    Revert "maybe if the context changes in the binding, it will rebind automatically"
    This reverts commit 2a273e4b59554db12a11ced35af8542de36d51ee.

--- OPrime-HTML5/montage-client/test/core/contextualizer.js
+++ OPrime-HTML5/montage-client/test/core/contextualizer.js
@@ -21,22 +21,21 @@ TestPageLoader.queueTest("../audio/audio-test", function(testPage) {
                         }
                     }
                 },
-                localize: function(context, key) {
-                    return this.data[context][key].message;
+                localize: function(key) {
+                    return this.data[this.currentLocale][key].message;
                 }
             }
         };
-        var currentContext = "context1";
         bind(object, "average", {
-            "<-": "localizer.localize(currentContext, 'contextualized_string')"
+            "<-": "localizer.localize('contextualized_string')"
         });
         expect(object.average).toEqual("this one is for context 1");
-        currentContext = "context2";
+        object.localizer.currentLocale = "context2";
 
         //TODO without binding the second time (re-binding) the test fails
-        bind(object, "average", {
-            "<-": "localizer.localize(currentContext, 'contextualized_string')"
-        });
+        // bind(object, "average", {
+        //     "<-": "localizer.localize('contextualized_string')"
+        // });
         expect(object.average).toEqual("this one is for context 2");
     });
 
