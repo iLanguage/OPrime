43a3671817e11818886dc4776c66838159b85fab
cesine <cesine@yahoo.com>
2014-02-13 18:03:37 -0500

    making the cordova audio non-global

--- OPrime-HTML5/montage-client/lib/cordova-bridge.js
+++ OPrime-HTML5/montage-client/lib/cordova-bridge.js
@@ -1,42 +1,40 @@
-// Wait for device API libraries to load
-//
-document.addEventListener("deviceready", onDeviceReady, false);
-
+var CordovaAudio = {};
 // device APIs are available
 //
-function onDeviceReady() {
-    playAudio("assets/pageflip2.mp3");
+CordovaAudio.onDeviceReady = function() {
+    this.play("assets/gammatone.wav");
 }
 
 // Audio player
 //
-var my_media = null;
-var mediaTimer = null;
+CordovaAudio.my_media = null;
+CordovaAudio.mediaTimer = null;
+CordovaAudio.library = "Cordova";
 
 // Play audio
 //
-function playAudio(src) {
+CordovaAudio.play = function(src) {
     // Create Media object from src
-    my_media = new Media(src, onSuccess, onError);
+    this.my_media = new Media(src, this.onSuccess, onError);
 
     // Play audio
-    my_media.play();
+    this.my_media.play();
 
-    // Update my_media position every second
-    if (mediaTimer == null) {
-        mediaTimer = setInterval(function() {
-            // get my_media position
-            my_media.getCurrentPosition(
+    // Update this.my_media position every second
+    if (this.mediaTimer == null) {
+        this.mediaTimer = setInterval(function() {
+            // get this.my_media position
+            this.my_media.getCurrentPosition(
                 // success callback
                 function(position) {
                     if (position > -1) {
-                        setAudioPosition((position) + " sec");
+                        this.setAudioPosition((position) + " sec");
                     }
                 },
                 // error callback
                 function(e) {
                     console.log("Error getting pos=" + e);
-                    setAudioPosition("Error: " + e);
+                    this.setAudioPosition("Error: " + e);
                 }
             );
         }, 1000);
@@ -45,37 +43,43 @@ function playAudio(src) {
 
 // Pause audio
 //
-function pauseAudio() {
-    if (my_media) {
-        my_media.pause();
+CordovaAudio.pause = function() {
+    if (this.my_media) {
+        this.my_media.pause();
     }
 }
 
 // Stop audio
 //
-function stopAudio() {
-    if (my_media) {
-        my_media.stop();
+CordovaAudio.stop = function() {
+    if (this.my_media) {
+        this.my_media.stop();
     }
-    clearInterval(mediaTimer);
-    mediaTimer = null;
+    clearInterval(this.mediaTimer);
+    this.mediaTimer = null;
 }
 
 // onSuccess Callback
 //
-function onSuccess() {
-    console.log("playAudio():Audio Success");
+CordovaAudio.onSuccess = function() {
+    console.log("CordovaAudio.play():Audio Success");
 }
 
 // onError Callback
 //
-function onError(error) {
-    alert('code: ' + error.code + '\n' +
+CordovaAudio.onError = function(error) {
+    console.warn('code: ' + error.code + '\n' +
         'message: ' + error.message + '\n');
 }
 
 // Set audio position
 //
-function setAudioPosition(position) {
-    document.getElementById('audio_position').innerHTML = position;
+CordovaAudio.setAudioPosition = function(position) {
+    if (document.getElementById('audio_position')) {
+        document.getElementById('audio_position').innerHTML = position;
+    }
 }
+
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready", CordovaAudio.onDeviceReady, false);
